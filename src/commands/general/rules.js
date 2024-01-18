8511fd0c4d19db7bd399725cd31344c63b02196b
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-18 23:15:05 +0200

    Create rules.js

---
+++ src/commands/general/rules.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'rule',
+    aliases: ['rules'],
+    category: 'general',
+    cool: 30,
+    react: "✅",
+    description: 'Bot rules',
+    async execute(client, arg, M) {
+
+        const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
+
+        let rules = `*Just dont abuse the bot or you will be banned*`
+        M.reply(rules)
+
+        
+    }
+}
