af134a16b96cfd9e1184333e1185e3e57a36fb32
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-19 00:17:21 +0200

    Create demote.js

---
+++ src/commands/group/demote.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'demote',
+    aliases: ['demo'],
+    react: "✅",
+    category: 'group',
+    description: 'Demotes the taged user',
+    async execute(client, arg, M) {
+
+   const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(` 🟥 *Bot is not enabled in current group ask mods to activate* `)
+        
+        if (!M.mentions.length) return M.reply('🟥 *You must tag the user to demote broh!*')
+        const groupMetadata = await client.groupMetadata(M.from)
+        const groupMembers = groupMetadata?.participants || []
+        const groupAdmins = groupMembers.filter((v) => v.admin).map((v) => v.id)
+        let adminUsers = []
+        // Filter the users who are admin and push the not admin users in the users var
+        M.mentions.filter((users) => (groupAdmins.includes(users) ? adminUsers.push(users) : null))
+        await client.groupParticipantsUpdate(M.from, adminUsers, 'demote').then((res) => {
+            M.reply(`✅ Done! Demoting ${adminUsers.length} users`)
+        })
+    }
+}
