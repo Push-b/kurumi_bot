aec7a42436ee620fa1112c856c5504836b799947
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-18 23:20:29 +0200

    Create eval.js

---
+++ src/commands/dev/eval.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'eval',
+    aliases: ['e'],
+    category: 'dev',
+    react: "✅",
+    description: 'Evaluates JavaScript',
+    async execute(client, arg, M) {
+       try{
+        if (!arg) return M.reply('🟥  *You didnt provided any term to eval!*')
+        let out = ''
+        try {
+            const output = (await eval(arg)) || '✅ *Executed JS Successfully!*'
+            out = JSON.stringify(output)
+        } catch (err) {
+            out = err.message
+        }
+        return await M.reply(out)
+       }catch(err){
+        await client.sendMessage(M.from , {image: {url: `${client.utils.errorChan()}`} , caption: `${client.utils.greetings()} Error-Chan Dis\n\nError:\n${err}`})
+      }
+    }
+}
