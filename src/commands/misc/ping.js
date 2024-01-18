ca503233877416c868c2226706e427de9d5c6848
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-19 00:26:46 +0200

    Create ping.js

---
+++ src/commands/misc/ping.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'ping',
+    aliases: ['speed'],
+    category: 'misc',
+    cool: 30,
+    react: "✅",
+    description: 'Bot response in second',
+    async execute(client, arg, M) {
+        await M.reply(`*_${client.utils.calculatePing(M.messageTimestamp, Date.now())} second(s)_*`)
+    }
+}
