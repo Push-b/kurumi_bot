2b042c9ee097b7ae327c691f882b3d314d4c22c4
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-19 00:20:30 +0200

    Create tagall.js

---
+++ src/commands/group/tagall.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: 'tagall',
+    aliases: ['everyone'],
+    react: "✅",
+    category: 'group',
+    description: 'Tag all the users present in the group',
+    async execute(client, arg, M) {
+
+   const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
+        
+        const groupMetadata = await client.groupMetadata(M.from)
+        const groupMembers = groupMetadata?.participants.map((x) => x.id) || []
+        const groupAdmins = groupMetadata.participants.filter((x) => x.admin).map((x) => x.id)
+
+        let text = `${arg !== '' ? `🧧 *Message: ${arg}*\n\n` : ''}🍀 *Group:* ${
+            groupMetadata.subject
+        }\n🎈 *Members:* ${groupMetadata.participants.length}\n📣 *Tagger: @${M.sender.split('@')[0]}*\n`
+
+        const admins = []
+        const members = []
+
+        for (const jid of groupMembers) {
+            if (groupAdmins.includes(jid)) {
+                admins.push(jid)
+                continue
+            }
+            members.push(jid)
+        }
+
+        // for (let i = 0; i < admins.length; i++) text += `${i === 0 ? '\n\n' : '\n'}🌟 *@${admins[i].split('@')[0]}*`
+        // for (let i = 0; i < members.length; i++) text += `${i === 0 ? '\n\n' : '\n'}🎗 *@${members[i].split('@')[0]}*`
+
+        await client.sendMessage(M.from, { text, mentions: groupMembers }, { quoted: M })
+    }
+}
