24fdd268d9a6fedc26787548eb62091f6e105621
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-19 00:24:33 +0200

    Create alive.js

---
+++ src/commands/misc/alive.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'alive',
+    aliases: ['a'],
+    category: 'misc',
+    cool: 30,
+    react: "✅",
+    description: 'Testing stuff',
+    async execute(client, arg, M) {
+        
+        const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
+        
+        M.reply(
+            `Everything is working l guess ${
+                (await client.contact.getContact(M.sender, client)).username
+            } | *Exp:* ${await client.exp.get(M.sender)} *Level:* ${(await client.DB.get(`${M.sender}_LEVEL`)) || 0}`
+        )
+    }
+}
