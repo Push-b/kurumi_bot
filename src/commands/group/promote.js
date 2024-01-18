f9dbe7dbe7d31097decd78e259e7f3f1cf3d4a44
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-19 00:19:15 +0200

    Create promote.js

---
+++ src/commands/group/promote.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'promote',
+    aliases: ['prom'],
+    react: "✅",
+    category: 'group',
+    description: 'Promotes the taged user',
+    async execute(client, arg, M) {
+
+   const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
+        
+        if (!M.mentions.length) return M.reply('🟥 *You must tag the user you want to make an admin!*')
+        const groupMetadata = await client.groupMetadata(M.from)
+        const groupMembers = groupMetadata?.participants || []
+        const groupAdmins = groupMembers.filter((v) => v.admin).map((v) => v.id)
+        let NotadminUsers = []
+        // Filter the users who are admin and push the not admin users in the users var
+        M.mentions.filter((users) => (groupAdmins.includes(users) ? null : NotadminUsers.push(users)))
+        await client.groupParticipantsUpdate(M.from, NotadminUsers, 'promote').then((res) => {
+            M.reply(`✅ Done! Promoting ${NotadminUsers.length} users`)
+        })
+    }
+}
