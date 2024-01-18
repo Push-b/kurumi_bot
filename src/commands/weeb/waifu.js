22399d6032f7dc053cb37156dd85211c56eec7a2
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-19 00:44:33 +0200

    Create waifu.js

---
+++ src/commands/weeb/waifu.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+
+module.exports = {
+    name: 'waifu',
+    aliases: ['animegirl'],
+    category: 'weeb',
+    cool: 10,
+    react: "✅",
+    description: 'Sends an image of a random waifu',
+    async execute(client, arg, M) {
+
+   const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
+        
+        const res = await axios.get(`https://api.waifu.im/search/?included_tags=waifu`).catch((err) => {
+            return M.reply(err.toString())
+            client.log(err, 'red')
+        })
+
+        client.sendMessage(M.from, {
+            image: {
+                url: res.data.images[0].url
+            },
+            caption: `Waifu from ${res.data.images[0].source}`
+        },
+        {
+            quoted: M
+        }
+        )
+    }
+}
