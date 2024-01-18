80faf0267feb621f68bbe00254b01c4d5d2899c1
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-18 23:23:46 +0200

    Create switch.js

---
+++ src/commands/dev/switch.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: 'switch',
+    aliases: ['promod'],
+    react: "✅",
+    category: 'dev',
+    description: 'Promotes the taged user',
+
+    async execute(client, arg, M) {
+
+        const toggleableGroupActions = ['archer','economy','media','naughty','card-game']
+
+        if (!arg)
+
+            return M.reply(
+
+                `Please provide a valid toggleable GroupActions\n\n*Available:* \n${toggleableGroupActions.join('\n')}`
+
+            )
+
+        if (!toggleableGroupActions.includes(arg.trim()))
+
+            return M.reply(
+
+                `🟥 *Please provide a valid toggleable GroupActions*\n\n*Available:* \n${toggleableGroupActions.join('\n')}`
+
+            )
+
+        const Actives = (await client.DB.get(arg)) || []
+
+        if (Actives.includes(M.from))
+
+            return M.reply(`✅ *${client.utils.capitalize(arg)} is already activate in your group*`)
+
+        await client.DB.push(arg, M.from)
+
+        M.reply(`✅ *Successfully Enabled ${client.utils.capitalize(arg)} in your group*`)
+
+    }
+
+}
