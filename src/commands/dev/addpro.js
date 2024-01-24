b0f537c1c6dac5963fc9e05855fddae8eb173648
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-25 00:03:04 +0200

    Create addpro.js

---
+++ src/commands/dev/addpro.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: 'addmods',
+  aliases: ['addmod'],
+  category: 'dev',
+  react: '✅',
+  description: 'Adds a new mod to the bot',
+  async execute(client, arg, M) {
+      if (M.quoted?.participant) M.mentions.push(M.quoted.participant)
+          
+          if (!M.mentions.length) return M.reply('🟥 *Mention a user you want to add as a proUser sir!*')
+    const mention = M.mentions[0]
+
+  //   if(!mention) return M.reply(``)
+
+    // Add the new proUser to the list
+    client.proUser.push(mention.split('@')[0]);
+
+    // Send a confirmation message
+
+    M.reply(`*Successfully added @${(await client.contact.getContact(mention, client)).username} as a proUser*`);
+  }
+};
