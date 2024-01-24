05b13598afe5a40ec5c95022b3a2b1839b89a739
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-24 21:23:54 +0200

    Create bankrupt.js

---
+++ src/commands/economy/bankrupt.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'bankrupt',
+    category: 'economy',
+    exp: 5,
+    cool: 10,
+    react: "💸",
+    description: 'Declares bankruptcy and resets your bank account',
+    async execute(client, arg, M) {
+
+      if (!arg) {
+        return await M.reply('Are you sure you want to declare bankruptcy? This will reset your bank account. Type `.yes` to confirm.')
+      }
+      
+      if (arg.startsWith('.yes') || arg.startsWith('.y')) {
+      await client.DB.sub(`${M.sender}.wallet`) || 0
+      M.reply('You have declared bankruptcy. Your bank account has been reset to 0 coins.')
+      } else {
+        M.reply('Cancelled bankruptcy declaration')
+      }
+      
+  }
+}
