2ab11d1c9d4e2a22f2d78b7f4667d5dc2eef5532
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-24 14:39:44 +0200

    Create burnttrast.js

---
+++ src/commands/rpg/burnttrast.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'burntrash',
+    aliases: ['destroy'],
+    category: 'rpg',
+    react: "✅",
+    description: 'Destroy a specified item from the inventory',
+    async execute(client, arg, M) {
+        if (!arg) return M.reply('Please specify an item to destroy')
+        const burnable = ['sword', 'armor', 'fishingrod', 'pickaxe']
+        if (!burnable.includes(arg)) return M.reply('That item does not exist in game.')
+        const inventory = await client.rpg.get(M.sender)
+        if (!(arg in inventory)) return M.reply('You do not have that item in your inventory.')
+        await client.rpg.delete(`${M.sender}[${arg}]`)
+        const random = Math.floor(Math.random() * 50) + 1
+        await client.cradit.get(`${M.sender}.wallet`) || 0
+        return M.reply(
+            `You destroyed ${arg} from your inventory. You recived ${random} yens as a result of this action`
+        )
+    }
+}
