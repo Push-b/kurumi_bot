9a15b073b8057b75bf440f82337f84b6171322c1
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-25 00:00:07 +0200

    Create mods.js

---
+++ src/commands/general/mods.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  name: 'mods',
+
+  aliases: ['mod'],
+
+  category: 'general',
+  
+//  cool: 4,
+
+  react: "✅",
+
+  description: 'Get information bot information',
+
+  async execute(client, arg, M) {
+
+  let mods = client.mods
+
+  let mo=`*❯──「 Archer Moderators 」──❮*\n\n`
+
+  for(let i=0;i<mods.length;i++){
+         
+  let hmm = mods[i]
+         
+ const um= (await client.contact.getContact(hmm, client)).username;
+         
+  mo+=`\n#${i+1}\n*Contact:* http://wa.me/+${mods[i]}\n`
+   }
+     M.reply(mo)
+  }
+}
