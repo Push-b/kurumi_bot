e3f1b56dc30194f9013d812151701d8529a4c3a9
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-24 21:41:34 +0200

    Update bankrupt.js

--- src/commands/economy/bankrupt.js
+++ src/commands/economy/bankrupt.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Declares bankruptcy and resets your bank account',
     async execute(client, arg, M) {
       
-      await client.DB.delete(`${M.sender}.wallet`) || 0
+      await client.cradit.delete(`${M.sender}.wallet`) || 0
       M.reply('You have declared bankruptcy. Your bank account has been reset to 0 dollars.')
       }
    }
