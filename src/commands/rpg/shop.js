7a89f021f7954d3ba5be7ab5c134f8796e560b4a
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-25 21:21:47 +0200

    Update shop.js

--- src/commands/rpg/shop.js
+++ src/commands/rpg/shop.js
@@ -4,8 +4,6 @@ const items = {
         { iron: 10000 },
         { pickaxe: 90000 },
         { gold: 37500 },
-        { sword: 90000 },
-        { armor: 37500 },
         { wood: 3000 },
         { string: 1000 },
         { diamond: 61500 },
