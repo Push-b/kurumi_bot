e076c31dad61df70abbd1e461859c0291a6a0a75
Push-b <153626742+Push-b@users.noreply.github.com>
2024-01-25 14:21:08 +0200

    Update inventory.js

--- src/commands/rpg/inventory.js
+++ src/commands/rpg/inventory.js
@@ -22,7 +22,7 @@ module.exports = {
         if (!invemtory) return M.reply('You have no inventory')
         let text = '*━━━❰🌀YOUR_INVENTORY🌀❱━━━*\n\n'
         for (const [key, value] of Object.entries(invemtory)) {
-            text += `>>> *${key}:* ${typeof value == 'number' ? value : '\n' + objToString(value)}\n`
+            text += `>>> *${key}:* ${typeof value == 'number' ? value : + objToString(value)}`
         }
         M.reply(text)
     }
