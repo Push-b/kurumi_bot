8f7243dd74060609bd4adbfd029700ed45357e1d
Push-b <153626742+Push-b@users.noreply.github.com>
2024-02-10 14:56:05 +0200

    Update inventory.js

--- src/commands/rpg/inventory.js
+++ src/commands/rpg/inventory.js
@@ -18,12 +18,32 @@ module.exports = {
     async execute(client, arg, M) {
      const rpg = (await client.DB.get('rpg')) || []
    if (!rpg.includes(M.from)) return M.reply(` *🟥 rpg is not enabled in current group ask mods to activate* `)
-        const invemtory = await client.rpg.get(M.sender)
-        if (!invemtory) return M.reply('You have no inventory')
-        let text = '*━━━❰🌀YOUR_INVENTORY🌀❱━━━*\n\n'
-        for (const [key, value] of Object.entries(invemtory)) {
-            text += `>>> *${key}:* ${typeof value == 'number' ? value : '\n\n' + objToString(value)}\n\n`
-        }
-        M.reply(text)
+
+  const thumbnailUrls = [
+    'https://telegra.ph/file/505307775b32d70bb432e.jpg',
+    'https://telegra.ph/file/505307775b32d70bb432e.jpg',
+    'https://telegra.ph/file/505307775b32d70bb432e.jpg',
+];
+
+function getRandomThumbnailUrl() {
+    const randomIndex = Math.floor(Math.random() * thumbnailUrls.length);
+    return thumbnailUrls[randomIndex];
+}
+  const thumbnailUrl = getRandomThumbnailUrl(); 
+  const invemtory = await client.rpg.get(M.sender)
+  if (!invemtory) return M.reply('You have no inventory')
+  for (const [key, value] of Object.entries(invemtory)) {
+  await client.sendMessage(
+          M.from, {
+          text: `*━━━❰🌀YOUR_INVENTORY🌀❱━━━*\n\n>>> *${key}:* ${typeof value == 'number' ? value : '\n\n' + objToString(value)}`,
+         contextInfo: {
+         externalAdReply: {
+         tittle: 'Wallet', 
+         body: '',
+        thumbnail: await client.utils.getBuffer(thumbnailUrl),
+        mediaType: 1
+            }
+         }
+      })
     }
-    }
+  }
