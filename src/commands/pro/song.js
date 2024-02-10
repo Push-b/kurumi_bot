f84ddbebf7384ac68862b90161a24b06b75b9d02
Push-b <153626742+Push-b@users.noreply.github.com>
2024-02-11 00:30:20 +0200

    Update song.js

--- src/commands/pro/song.js
+++ src/commands/pro/song.js
@@ -33,7 +33,7 @@ module.exports = {
        contextInfo:{
         externalAdReply:{
        Title: 'videoDetails.title',
-       body: 'B  Y      D  E  R  Y  L',
+       body: '🎧< D O C U M E N T    A U D I O >🎧',
       thumbnail:  await client.utils.getBuffer(`https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`),
       mediaType:2,
           }
