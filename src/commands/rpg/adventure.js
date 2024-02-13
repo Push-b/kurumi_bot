3abc9ebc209731857bffa565cb13485ab2a2ba81
Push-b <153626742+Push-b@users.noreply.github.com>
2024-02-13 20:20:34 +0200

    Update adventure.js

--- src/commands/rpg/adventure.js
+++ src/commands/rpg/adventure.js
@@ -8,8 +8,6 @@ const reward = (level) => {
         reward: {
             trash: Math.round(Math.random() * 5) * level,
             potion: Math.round(Math.random() * 2) * level,
-            armor: Math.round(Math.random() * 3) * level,
-            sword: Math.round(Math.random() * 5) * level,
             wood: Math.round(Math.random() * 8) * level,
             string: Math.round(Math.random() * 8) * level,
             iron: Math.round(Math.random() * 5) * level,
@@ -27,8 +25,6 @@ module.exports = {
     exp: 8,
     description: 'RPG games for adventure',
     async execute(client, arg, M) {
-    const rpg = (await client.DB.get('rpg')) || []
-   if (!rpg.includes(M.from)) return M.reply(` *🟥 rpg is not enabled in current group ask mods to activate* `)
         const cooldown = 300000
         const lastadvn = await client.DB.get(`${M.sender}.adventure`)
         if (lastadvn !== null && cooldown - (Date.now() - lastadvn) > 0) {
@@ -39,8 +35,8 @@ module.exports = {
         }
         const level = await client.DB.get(`${M.sender}_LEVEL`) || 1
         const health = await client.rpg.get(`${M.sender}.health`) || 100
-        const armor = await client.rpg.get(`${M.sender}.armor`)
-        const sword = await client.rpg.get(`${M.sender}.sword`)
+        const armor = await client.rpg.get(`${M.sender}.armor.durability`)
+        const sword = await client.rpg.get(`${M.sender}.sword.durability`)
         if (!armor) return M.reply(`*You dont have any armor!!*`)
         if (!sword) return M.reply(`*You dont have a sword!!*`)
         if (health < 30) return M.reply(`*You dont have the required health ❤️*`)
@@ -51,13 +47,13 @@ module.exports = {
                 armor
             )}, ${percentageCal(5, sword)} respectively damage*`
         )
-        await client.rpg.sub(`${M.sender}.armor`, percentageCal(40, armor))
-        await client.rpg.set(`${M.sender}.health`, health - percentageCal(30, health))
-        await client.rpg.sub(`${M.sender}.sword`, percentageCal(10, sword))
-        let text = '🔖 *Your Adventure Reward:*\n\n'
+        await client.rpg.sub(`${M.sender}.armor.durability`, percentageCal(30, armor))
+        await client.rpg.set(`${M.sender}.health`, health - percentageCal(10, health))
+        await client.rpg.sub(`${M.sender}.sword.durability`, percentageCal(5, sword))
+        let text = '🔖 *Adventure Rewards:*\n\n'
         for (const rewardItem in reward(level).reward) {
             await client.rpg.add(`${M.sender}[${rewardItem}]`, reward(level).reward[rewardItem])
-            text += `*>>> ${rewardItem}: _${reward(level).reward[rewardItem]}_*\n`
+            text += `*> ${rewardItem}: _${reward(level).reward[rewardItem]}_*\n`
         }
         setTimeout(() => {
             M.reply(text)
