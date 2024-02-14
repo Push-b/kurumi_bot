7455aa84d67650a5b22c573fdadf2ed6c6ad01d5
Push-b <153626742+Push-b@users.noreply.github.com>
2024-02-14 21:09:10 +0200

    Create define.js

---
+++ src/commands/fun/define.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const fs=require("fs")
+
+module.exports={
+    name:"define",
+    alias:['dictionary'],
+    usage:`${prefa}define [Your word]`,
+    desc:"Gives you the meaning of your word ",
+    category:"Education",
+    react:"📖",
+    async execute(client, arg, M) {
+if (!arg) return m.reply(`Please give me text.`)
+ const  data  = await axios.get(`https://urban-dictionary-api.0xn1nja.repl.co/api?word=${arg}`);
+const res = data.data
+    let img = res.mug_back_image;
+    // const { definition, example } = data.list[0];
+    const reply = `
+*🔠 Word:* ${arg}
+*📖 Definition:* ${res.meaning.replace(/\[|\]/g, '')}
+*💭 Example:* ${res.example.replace(/\[|\]/g, '')}
+    `;
+ await client.sendMessage(M.from, {image: {url: img} ,caption:reply},{quoted:M})
+  })
+ .catch(err)(
+await client.sendMessage(M.from , {image: {url: `${client.utils.errorChan()}`} , caption: `${client.utils.greetings()} Error-Chan Dis\n${err}`})
+            )
+        }
+    }
+}
