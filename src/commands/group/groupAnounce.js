2d8f8152675e0d64b2d9b5c88125713e30bbfb78
Push-b <153626742+Push-b@users.noreply.github.com>
2024-02-14 20:15:35 +0200

    Create groupAnounce.js

---
+++ src/commands/group/groupAnounce.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'group',
+    aliases: ['gc'],
+    exp: 5,
+    react: "✅",
+    category: 'moderation',
+    description: 'Closes or opens the group',
+    async execute(client, arg, M) {
+        const group = ['open', 'close']
+        if (!arg) return M.reply('Sorry you did not give any term!')
+        if (!group.includes(arg)) return M.reply('Sorry you did not give any valid term!')
+        const groupMetadata = await client.groupMetadata(M.from)
+        switch (arg) {
+            case 'open':
+                if (!groupMetadata.announce) return M.reply('Already opened!')
+                await client.groupSettingUpdate(M.from, 'not_announcement')
+                return M.reply('Group opened')
+                break
+            case 'close':
+                if (groupMetadata.announce) return M.reply('Already closed!')
+                await client.groupSettingUpdate(M.from, 'announcement')
+                return M.reply('Group Closed')
+                break
+        }
+    }
+}
