27725e286022cab06e215706a188244e721252b4
Push-b <153626742+Push-b@users.noreply.github.com>
2024-02-14 22:01:39 +0200

    Update define.js

--- src/commands/fun/define.js
+++ src/commands/fun/define.js
@@ -10,20 +10,20 @@ module.exports={
     async execute(client, arg, M) {
 if (!arg) return M.reply(`Please give me text.`)
 try {
-    const  data  = await axios.get(`https://urban-dictionary-api.0xn1nja.repl.co/api?word=${q}`);
+    const  data  = await axios.get(`https://urban-dictionary-api.0xn1nja.repl.co/api?word=${arg}`);
 
 const res = data.data
     let img = res.mug_back_image;
     // const { definition, example } = data.list[0];
     const reply = `
-*🔠 Word:* ${q}
+*🔠 Word:* ${arg}
 *📖 Definition:* ${res.meaning.replace(/\[|\]/g, '')}
 *💭 Example:* ${res.example.replace(/\[|\]/g, '')}
     `;
    client.sendMessage(M.from,{image: {url: img} ,caption:reply},{quoted:M})
 } catch (err) { 
     console.log(err)
-    return M.reply (`*${q}* isn't a valid text`)
+    return M.reply (`*${arg}* isn't a valid text`)
     }
   }
 }
