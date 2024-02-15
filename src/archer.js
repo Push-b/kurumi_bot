8bcaff42ce552ef33fa93c4c841e3321d12bb2ce
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-15 23:23:23 +0200

    Update archer.js

--- src/archer.js
+++ src/archer.js
@@ -12,7 +12,7 @@ const { MongoDriver } = require('quickmongo')
 const { Collection } = require('discord.js')
 const auth = require("./handlers/auth")
 const MessageHandler = require('./handlers/Message')
-const CardHandler = require('./Handlers/card')
+const CardHandler = require('./handlers/card')
    // call the summon function
 const jid = "120363043742977407@g.us";
 
