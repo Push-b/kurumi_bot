84ef1cbef4d2f6ca1613e61f27ad559232317f17
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-16 17:09:05 +0200

    Update video.js

--- src/commands/pro/video.js
+++ src/commands/pro/video.js
@@ -4,17 +4,14 @@ const yts = require("youtube-yts");
 module.exports = {
     name: 'video',
     aliases: ['video'],
-    category: 'media',
+    category: 'proUsers',
     description: 'Downloads given YT Video',
     react: "✅",
     async execute(client, arg, M) {
-  const link = async (term) => {
-      
-     const videotoken = await client.media.get(`${M.sender}.videotoken`)
-     if (!videotoken) return M.reply(`🟥 You dont have any video token visit the *.media-shop* and buy video tokens!`)
-     await client.media.sub(`${M.sender}.videotoken`, 1)
-    
-      const link = async (term) => {
+          const archer = (await client.DB.get('archer')) || []
+   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
+        
+        const link = async (term) => {
             const { videos } = await yts(term.trim())
             if (!videos || !videos.length) return null
             return videos[0].url
@@ -25,7 +22,20 @@ module.exports = {
         if (!term) return M.reply('Please use this command with a valid youtube content link')
         if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
         const { videoDetails } = await YT.getInfo(term)
-        M.reply('*sending....*')
+        M.reply('*🎧...downloading...🎧*')
+        let text = `*Title:* ${videoDetails.title} | *Type:* Video | *From:* ${videoDetails.ownerChannelName}`
+        client.sendMessage(
+            M.from,
+            {
+                image: {
+                    url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
+                },
+                caption: text
+            },
+            {
+                quoted: M
+            }
+        )
         if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download video longer than 30 minutes')
         const audio = YT.getBuffer(term, 'video')
             .then(async (res) => {
