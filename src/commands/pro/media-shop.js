e3879bd7a11b8a2988fc0bca90e8b7425ea0e627
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-16 14:56:13 +0200

    Update media-shop.js

--- src/commands/pro/media-shop.js
+++ src/commands/pro/media-shop.js
@@ -1,5 +1,5 @@
 const items = {
-    buy: [
+    purchase: [
         { musictoken: 10000 },
         { tiktoktoken: 10000 },
         { lyricstoken: 1000 },
@@ -16,12 +16,12 @@ module.exports = {
     react: "✅",
     description: 'Buy media tokens',
     async execute(client, arg, M) {
-        const command = M.body.split(' ')[0].toLowerCase().slice(client.prefix.length).trim()
+     const command = M.body.split(' ')[0].toLowerCase().slice(client.prefix.length).trim()
         if (command == 'media-shop') {
             const typeEmoji = ['🛍️','⚖️']
             const moneyEmoji = ['💵','💵']
             const types = Object.keys(items)
-            let text = '*━━━━❰⭐Media tokens shop⭐❱━━━━*'
+            let text = '*━━━━❰⭐Media token SHOP⭐❱━━━━*'
             for (const type of types) {
                 text += `\n\n*${typeEmoji[types.indexOf(type)]} ${client.utils.capitalize(type)}*\n`
                 items[type].filter((x) => {
@@ -30,21 +30,21 @@ module.exports = {
                     }
                 })
             }
-            text += `\n\n🧧 Use ${client.prefix}purchase <item_name>`
+            text += `\n\n🧧 Use ${client.prefix}buy <item_name>`
             M.reply(text)
         }
         if (command == 'purchase') {
-            if (!arg) return M.reply('Please give a item name you want to purchase')
+            if (!arg) return M.reply('Please give a item name')
             const term = arg.split(' ')
             const purchaseItems = Object.keys(Object.assign({}, ...items[command]))
-            if (!purchaseItems.includes(term[0].toLowerCase())) return M.reply('Please provide a valid media type to purchase your token')
+            if (!purchaseItems.includes(term[0].toLowerCase())) return M.reply('Please give a valid item name')
             const cradits = (await client.cradit.get(`${M.sender}.wallet`)) || 0
             const price =
                 parseInt(Object.values(items[command][buyItems.indexOf(term[0].toLowerCase())]).join('')) *
                 (term[1] || 1)
             if ((cradits - price) < 0)
                 return M.reply(`You dont have that much in your wallet to buy ${term[0].toLowerCase()} ${term[1] || 1}`)
-            await client.rpg.add(`${M.sender}[${term[0].toLowerCase()}]`, 10 * parseInt(term[1] || 1))
+            await client.rpg.add(`${M.sender}[${term[0].toLowerCase()}]`, 1 * parseInt(term[1] || 1))
             await client.cradit.sub(`${M.sender}.wallet`, price)
             M.reply(
                 `*Thank you 🎉 for your purches*\n*Now you have _${client.utils.capitalize(term[0])} : ${(await client.rpg.get(`${M.sender}[${term[0].toLowerCase()}]`)) || 0
