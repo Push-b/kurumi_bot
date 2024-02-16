493132156ce1a130662be219ab2ca9e31482072a
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-16 17:20:13 +0200

    Update video.js

--- src/commands/pro/video.js
+++ src/commands/pro/video.js
@@ -11,9 +11,6 @@ module.exports = {
 
         
         const link = async (term) => {
-     const videotoken = await client.media.get(`${M.sender}.videotoken`)
-     if (!videotoken) return M.reply(`🟥 You dont have any music token visit the *.media-shop* and buy music tokens!`)
-     await client.media.sub(`${M.sender}.videotoken`, 1)
             const { videos } = await yts(term.trim())
             if (!videos || !videos.length) return null
             return videos[0].url
@@ -24,6 +21,9 @@ module.exports = {
         if (!term) return M.reply('Please use this command with a valid youtube content link')
         if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
         const { videoDetails } = await YT.getInfo(term)
+        const videotoken = await client.media.get(`${M.sender}.videotoken`)
+     if (!videotoken) return M.reply(`🟥 You dont have any music token visit the *.media-shop* and buy music tokens!`)
+     await client.media.sub(`${M.sender}.videotoken`, 1)
         M.reply('*🎧...downloading...🎧*')
         let text = `*Title:* ${videoDetails.title} | *Type:* Video | *From:* ${videoDetails.ownerChannelName}`
         client.sendMessage(
