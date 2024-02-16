de1bae5484e2dae5425d5a9d5eeb6b51b650f73e
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-16 15:49:02 +0200

    Update tokens.js

--- src/commands/pro/tokens.js
+++ src/commands/pro/tokens.js
@@ -18,10 +18,10 @@ module.exports = {
     async execute(client, arg, M) {
 
   const thumbnailUrls = [
-    'https://telegra.ph/file/a0210abcd2451da334fca.jpg',
-    'https://telegra.ph/file/2392a86e06ae7ff992961.jpg',
-    'https://telegra.ph/file/c36b704c038623ec5a21d.jpg',
-    'https://telegra.ph/file/8a1c35f16bb06c210b8da.jpg',
+    'https://telegra.ph/file/e8a83694a1884505bc6a2.jpg',
+    'https://telegra.ph/file/874e9f4c743c7081294c4.jpg',
+    'https://telegra.ph/file/c6cb6c95679faf56e014c.jpg',
+    'https://telegra.ph/file/00cdb5f54623e1b1f3de4.jpg',
 ];
 
 function getRandomThumbnailUrl() {
@@ -34,11 +34,11 @@ function getRandomThumbnailUrl() {
   for (const [key, value] of Object.entries(tokens)) {
   await client.sendMessage(
           M.from, {
-          text: `*>>>  ${key}:* ${typeof value == 'number' ? value : '\n\n' + objToString(value)}`,
+          text: `➜  ${key}:* ${typeof value == 'number' ? value : '\n\n' + objToString(value)}`,
          contextInfo: {
          externalAdReply: {
          tittle: 'Wallet', 
-         body: 'T   O   K   E   N   S',
+         body: '🏖️  T   O   K   E   N   S   🏖️',
         thumbnail: await client.utils.getBuffer(thumbnailUrl),
         mediaType: 1
             }
