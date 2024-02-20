e0e142d466c43efb3826fbe12746b024e8872de1
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-20 23:30:47 +0200

    Update Message.js

--- src/handlers/Message.js
+++ src/handlers/Message.js
@@ -31,8 +31,6 @@ module.exports = MessageHandler = async (messages, client) => {
         const banned = (await client.DB.get('banned')) || []
         const getCard = (await client.DB.get('cards')) || []
         const cardgame = (await client.DB.get('cardgame')) || []
-           // call the summon function
-        const jid = '120363043742977407@g.us';
         const auction = (await client.DB.get('auction')) || []
         const cshop = (await client.DB.get('cshop')) || []
         const economy = (await client.DB.get('economy')) || []
