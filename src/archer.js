b937633aaa33b01265cb1b701321960547695a44
Deryl <153626742+Push-b@users.noreply.github.com>
2024-02-23 21:01:51 +0200

    Update archer.js

--- src/archer.js
+++ src/archer.js
@@ -16,7 +16,7 @@ const CardHandler = require('./handlers/card')
    // call the summon function
 const jid = "27787800567-1632642032@g.us";
 
-const EventsHandler = require('./handlers/Events')
+const EventsHandler = require('./handlers/events')
 const contact = require('./lib/contacts')
 const gpt = require('./lib/gpt')
 const utils = require('./lib/function')
