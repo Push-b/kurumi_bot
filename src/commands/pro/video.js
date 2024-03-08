57b682837afdeaa6edede53a7d487e4998498fdb
Deryl <153626742+Push-b@users.noreply.github.com>
2024-03-08 09:46:47 +0200

    Update video.js

--- src/commands/pro/video.js
+++ src/commands/pro/video.js
@@ -2,49 +2,42 @@ const YT = require('../../lib/YT')
 const yts = require("youtube-yts");
 
 module.exports = {
-    name: 'video',
-    aliases: ['video'],
+    name: 'ytv',
+    aliases: ['ytv'],
     category: 'media',
+    exp: 5,
     description: 'Downloads given YT Video',
-    react: "✅",
     async execute(client, arg, M) {
-      const link = async (term) => {
+    const link = async (term) => {
             const { videos } = await yts(term.trim())
             if (!videos || !videos.length) return null
             return videos[0].url
-           }
-        if (!arg) return M.reply('🟥 Please use this command with a valid youtube.com link')
+        }
+        if (!arg) return M.reply('Please use this command with a valid youtube.com link')
         const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
         const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
-        if (!term) return M.reply('🟥 Please use this command with a valid youtube content link')
-        if (!YT.validateURL(term.trim())) return M.reply('🟥 Please use this command with a valid youtube.com link')
+        if (!term) return M.reply('Please use this command with a valid youtube content link')
+        if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
         const { videoDetails } = await YT.getInfo(term)
-        M.reply('*🎥 fetching your video.....*')
+        M.reply('*🎥 fetching your video....*')
         if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download video longer than 30 minutes')
         const audio = YT.getBuffer(term, 'video')
             .then(async (res) => {
- await client.sendMessage(M.from, { 
-         document: res,
-        fileName: videoDetails.title + '.mp4',
-        mimetype: 'video/mp4',
-       contextInfo:{
-        externalAdReply:{
-       Title: 'videoDetails.title',
-       body: videoDetails.title,
-      thumbnail:  await client.utils.getBuffer(`https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`),
-      mediaType: 2,
-          }
-       }
-    })
-  })
-
- .catch((err) => {
-return M.reply(err.toString())
-client.log(err, 'red')
+                await client.sendMessage(
+                    M.from,
+                    {
+                        document: res,
+                        mimetype: 'video/mp4',
+                        fileName: videoDetails.title + '.mp4'
+                    },
+                    {
+                        quoted: M
+                    }
+                )
+            })
+            .catch((err) => {
+                return M.reply(err.toString())
             })
     }
 }
 //M.quoted.mtype === 'imageMessage',
-
-        
-     
