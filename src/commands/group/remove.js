552ef143274c2ecfc3b33770a10b9c668f6179d1
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 11:58:59 +0200

    Update remove.js

--- src/commands/group/remove.js
+++ src/commands/group/remove.js
@@ -5,11 +5,8 @@ module.exports = {
     category: 'group',
     description: 'Removes the taged user',
     async execute(client, arg, M) {
-
-  const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
         
-        if (!M.mentions.length) return M.reply('🟥 *You must tag the user for me to remove!*')
+        if (!M.mentions.length) return M.reply('*You must tag the user for me to remove!*')
         await client.groupParticipantsUpdate(M.from, M.mentions, 'remove').then((res) => {
             M.reply(`✅ *Done! removing ${M.mentions.length} users*`)
         })
