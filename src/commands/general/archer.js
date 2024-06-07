911630d38a36e9c45a016ae2c0a488975589aa0c
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 12:02:48 +0200

    Update archer.js

--- src/commands/general/archer.js
+++ src/commands/general/archer.js
@@ -6,9 +6,6 @@ module.exports = {
     react: "✅",
     description: 'Bot faq',
     async execute(client, arg, M) {
-
-   const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
        
 
         let caption = `
