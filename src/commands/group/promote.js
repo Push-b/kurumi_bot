32742599fff1be7e7a17b0d4b52b4019e74379b5
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 11:59:41 +0200

    Update promote.js

--- src/commands/group/promote.js
+++ src/commands/group/promote.js
@@ -5,11 +5,8 @@ module.exports = {
     category: 'group',
     description: 'Promotes the taged user',
     async execute(client, arg, M) {
-
-   const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
         
-        if (!M.mentions.length) return M.reply('🟥 *You must tag the user you want to make an admin!*')
+        if (!M.mentions.length) return M.reply('*You must tag the user you want to make an admin!*')
         const groupMetadata = await client.groupMetadata(M.from)
         const groupMembers = groupMetadata?.participants || []
         const groupAdmins = groupMembers.filter((v) => v.admin).map((v) => v.id)
@@ -17,7 +14,7 @@ module.exports = {
         // Filter the users who are admin and push the not admin users in the users var
         M.mentions.filter((users) => (groupAdmins.includes(users) ? null : NotadminUsers.push(users)))
         await client.groupParticipantsUpdate(M.from, NotadminUsers, 'promote').then((res) => {
-            M.reply(`✅ Done! Promoting ${NotadminUsers.length} users`)
+            M.reply(`✅ Done! Promoting ${NotadminUsers.length}`)
         })
     }
 }
