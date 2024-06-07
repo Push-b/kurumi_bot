9e03a24772f75c9c7710f08a6571b346ceeb10e9
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 12:00:12 +0200

    Update grouplink.js

--- src/commands/group/grouplink.js
+++ src/commands/group/grouplink.js
@@ -5,12 +5,9 @@ module.exports = {
     category: 'group',
     description: 'Get the group link',
     async execute(client, arg, M) {
-
-    const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
         
         const invitelink = (await client.DB.get('invitelink')) || []
-        if (!invitelink.includes(M.from)) return M.reply(`🟥 *Invitelink is not registered on this group*`)
+        if (!invitelink.includes(M.from)) return M.reply(`*Invitelink is not registered on this group*`)
         const code = await client.groupInviteCode(M.from)
         M.reply('https://chat.whatsapp.com/' + code)
     }
