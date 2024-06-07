097d8fd327b30cb41c9fdcdd948b2a397374bd5a
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 12:01:13 +0200

    Update groupAnounce.js

--- src/commands/group/groupAnounce.js
+++ src/commands/group/groupAnounce.js
@@ -8,18 +8,18 @@ module.exports = {
     async execute(client, arg, M) {
         const group = ['open', 'close']
         if (!arg) return M.reply('Sorry you did not give any term!')
-        if (!group.includes(arg)) return M.reply('Sorry you did not give any valid term!')
+        if (!group.includes(arg)) return M.reply('*Sorry you did not give any valid term!*')
         const groupMetadata = await client.groupMetadata(M.from)
         switch (arg) {
             case 'open':
-                if (!groupMetadata.announce) return M.reply('Already opened!')
+                if (!groupMetadata.announce) return M.reply('*Already opened!*')
                 await client.groupSettingUpdate(M.from, 'not_announcement')
-                return M.reply('Group opened')
+                return M.reply('*Group opened*')
                 break
             case 'close':
-                if (groupMetadata.announce) return M.reply('Already closed!')
+                if (groupMetadata.announce) return M.reply('*Already closed!*')
                 await client.groupSettingUpdate(M.from, 'announcement')
-                return M.reply('Group Closed')
+                return M.reply('*Group Closed8*')
                 break
         }
     }
