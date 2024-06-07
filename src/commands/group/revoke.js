f6f96c4cc1cfd12ffdaa2b8572eec137b413e609
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 11:58:35 +0200

    Update revoke.js

--- src/commands/group/revoke.js
+++ src/commands/group/revoke.js
@@ -6,9 +6,6 @@ module.exports = {
     description: 'Resets group link',
     async execute(client, arg, M) {
         
-    const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
-        
         await client.groupRevokeInvite(M.from).then((res) => {
             M.reply(`✅ *Done! Group link has been reset*`)
         })
