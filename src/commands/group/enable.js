8a4ec16108bdfc67aba1eaa22b32ce7970d85f12
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-07 11:56:37 +0200

    Update enable.js

--- src/commands/group/enable.js
+++ src/commands/group/enable.js
@@ -5,9 +5,6 @@ module.exports = {
     category: 'group',
     description: 'Activate certain features on group-chats',
     async execute(client, arg, M) {
-
-    const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(`🟥 *Bot is not enabled in current group ask mods to activate* `)
         
         const toggleableGroupActions = ['mod', 'events' ]
         if (!arg)
@@ -20,7 +17,7 @@ module.exports = {
             )
         const Actives = (await client.DB.get(arg)) || []
         if (Actives.includes(M.from))
-            return M.reply(`${client.utils.capitalize(arg)} is already activate in your group`)
+            return M.reply(`✅ ${client.utils.capitalize(arg)} is already activate in your group`)
         await client.DB.push(arg, M.from)
         M.reply(`✅ Successfully activating ${client.utils.capitalize(arg)} in your group`)
     }
