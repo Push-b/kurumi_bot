2ce63fdf700deae581279365a93993b85983ae08
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-18 12:49:58 +0200

    well

--- src/commands/economy/give.js
+++ src/commands/economy/give.js
@@ -5,12 +5,6 @@ module.exports = {
     react: "✅",
     description: 'Transfer golds to your friend',
     async execute(client, arg, M) {
-
-     const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
-        
-    const economy = (await client.DB.get('economy')) || []
-     if (!economy.includes(M.from)) return M.reply(` *❌ Type .support to get Casino group* `)
       
         if (!M.mentions.length) return M.reply('*You must mention someone to give money*')
         const amount = parseInt(arg.split(' ')[0])
