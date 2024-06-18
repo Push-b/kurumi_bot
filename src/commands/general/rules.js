9b4f1edf3d54927e21a81d0bfd7d6911274b991f
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-18 16:21:51 +0200

    well

--- src/commands/general/rules.js
+++ src/commands/general/rules.js
@@ -6,9 +6,6 @@ module.exports = {
     description: 'Bot rules',
     async execute(client, arg, M) {
 
-        const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
-
         let rules = `*Just dont abuse the bot or you will be banned*`
         M.reply(rules)
 
