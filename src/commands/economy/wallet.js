47b243ceecf712b6916832aef03a5ec044c48075
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-18 12:50:44 +0200

    Update wallet.js

--- src/commands/economy/wallet.js
+++ src/commands/economy/wallet.js
@@ -6,12 +6,6 @@ module.exports = {
     description: 'Shows the wallet value',
     async execute(client, arg, M) {
 
-  const archer = (await client.DB.get('archer')) || []
-   if (!archer.includes(M.from)) return M.reply(` *🟥 Bot is not enabled in current group ask mods to activate* `)
-        
-    const economy = (await client.DB.get('economy')) || []
-     if (!economy.includes(M.from)) return M.reply(` *❌ Type .support to get Casino group* `)
-
     const thumbnailUrls = [
     'https://telegra.ph/file/9cd2636c54369e9ce47b1.jpg',
     'https://telegra.ph/file/3fcf43fcbf902bd6ac8a5.jpg',
