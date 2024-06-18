c25f317cf5118ae5fef77d71efb23fa30292ec9a
Deryl <153626742+Push-b@users.noreply.github.com>
2024-06-18 21:27:31 +0200

    Update help.js

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -52,7 +52,7 @@ module.exports = {
 ┣ *User: ${pushName}*
 ┣ *Prefix : << ${client.prefix} >>*
 ┣ *Owner: Deryl*
-╰────────────────\n\nThis help menu is designed to help you get started with the bot.\n\n⟾ *📪Command List📪*\n\n${commands}\n\n📚Notes: *➪Use ${client.prefix}help <command_name> for more info of a specific command.*\n*➪Example: ${client.prefix}help bank.*`;
+╰────────────────\n\nThis help menu is designed to help you get started with the bot.\n\n⟾ *📪Command List📪*\n\n${commands}\n📚Notes: *➪Use ${client.prefix}help <command_name> for more info of a specific command.*\n*➪Example: ${client.prefix}help bank.*`;
        
                 const imageUrls = [
                     'https://telegra.ph/file/08b8f4f754693568ce929.jpg',
